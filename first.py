8b917494c854816683ffe401c65d7a53a26cf96a
JonyMahith <jonymahith@gmail.com>
2023-11-05 00:35:41 +0530

    git commit -m "hand cricket basic code"

--- first.py
+++ first.py
@@ -1,18 +1,39 @@
 import cv2
 import numpy as np
 
-# Load the image
-image = cv2.imread("folder/WhatsApp Image 2023-11-04 at 9.48.23 PM.jpeg")
+# Load the image from the file
+img = cv2.imread('folder/finger scan.jpg')
 
-# Convert the image to grayscale
-# gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-# cv2.imshow(gray)
+# Check if the image was loaded successfully
+if img is not None:
+    # Define the desired width and height for the resized image
+    new_width = 800  # Change this value to your desired width
+    new_height = 600  # Change this value to your desired height
+
+    # Resize the image to the new dimensions
+    image = cv2.resize(cake, (new_width, new_height))
+
+    # Display the resized image in a window
+    cv2.imshow('image', image)
+
+    # Wait for a key press indefinitely
+    cv2.waitKey(0)
+
+    # Close all OpenCV windows
+    cv2.destroyAllWindows()
+else:
+    print("Error: Could not load the image.")
+
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 # Apply Gaussian blur to reduce noise
-blurred = cv2.GaussianBlur(image, (5, 5), 0)
+blurred = cv2.GaussianBlur(gray, (5, 5), 0)
 
 # Threshold the image to create a binary mask
 _, thresholded = cv2.threshold(blurred, 120, 255, cv2.THRESH_BINARY)
+
+# Convert the thresholded image to CV_8UC1 format
+thresholded = thresholded.astype(np.uint8)
 
 # Find contours in the binary mask
 contours, _ = cv2.findContours(thresholded, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
