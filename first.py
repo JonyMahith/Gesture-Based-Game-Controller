5663c79d2d7aae68c80e3971b540270eabd156e3
JonyMahith <jonymahith@gmail.com>
2023-11-05 07:10:01 +0530

    uptadted version of the video capture

--- first.py
+++ first.py
@@ -1,78 +1,47 @@
 import cv2
 import mediapipe as mp
-mp_drawing = mp.solutions.drawing_utils
-mp_drawing_styles = mp.solutions.drawing_styles
-mp_hands = mp.solutions.hands
 
-# For webcam input:
 cap = cv2.VideoCapture(0)
-with mp_hands.Hands(
-    model_complexity=0,
-    min_detection_confidence=0.5,
-    min_tracking_confidence=0.5) as hands:
-  while cap.isOpened():
-    success, image = cap.read()
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands()
+finger_coordinates = [(8, 6), (12, 10), (16, 14), (20, 10)]
+thumb_coordinate = (4, 2)
+mp_drawing = mp.solutions.drawing_utils
+
+while True:
+    success, img = cap.read()
     if not success:
-      print("Ignoring empty camera frame.")
-      # If loading a video, use 'break' instead of 'continue'.
-      continue
+        print("End of video.")
+        break
 
-    # To improve performance, optionally mark the image as not writeable to
-    # pass by reference.
-    image.flags.writeable = False
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    results = hands.process(image)
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+    multi_landmarks = results.multi_hand_landmarks
 
-    # Draw the hand annotations on the image.
-    image.flags.writeable = True
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    if multi_landmarks:
+        hand_points = []
+        for hand_lms in multi_landmarks:
+            mp_drawing.draw_landmarks(img, hand_lms, mp_hands.HAND_CONNECTIONS)
+            for idx, lm in enumerate(hand_lms.landmark):
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                hand_points.append((cx, cy))
 
-    # Initially set finger count to 0 for each cap
-    fingerCount = 0
+        for point in hand_points:
+            cv2.circle(img, point, 10, (0, 0, 255), cv2.FILLED)
 
-    if results.multi_hand_landmarks:
+        up_count = 0
 
-      for hand_landmarks in results.multi_hand_landmarks:
-        # Get hand index to check label (left or right)
-        handIndex = results.multi_hand_landmarks.index(hand_landmarks)
-        handLabel = results.multi_handedness[handIndex].classification[0].label
+        for coordinate in finger_coordinates:
+            if hand_points[coordinate[0]][1] < hand_points[coordinate[1]][1]:
+                up_count += 1
 
-        # Set variable to keep landmarks positions (x and y)
-        handLandmarks = []
+        cv2.putText(img, str(up_count), (100, 100), cv2.FONT_HERSHEY_SIMPLEX, 4, (255, 0, 0), 12)
 
-        # Fill list with x and y positions of each landmark
-        for landmarks in hand_landmarks.landmark:
-          handLandmarks.append([landmarks.x, landmarks.y])
+    cv2.imshow('Fingercounts', img)
 
-        
-        if handLabel == "Left" and handLandmarks[4][0] > handLandmarks[3][0]:
-          fingerCount = fingerCount+1
-        elif handLabel == "Right" and handLandmarks[4][0] < handLandmarks[3][0]:
-          fingerCount = fingerCount+1
+    if cv2.waitKey(5) & 0xFF == ord('q'):
+        break
 
-       
-        if handLandmarks[8][1] < handLandmarks[6][1]:       #Index finger
-          fingerCount = fingerCount+1
-        if handLandmarks[12][1] < handLandmarks[10][1]:     #Middle finger
-          fingerCount = fingerCount+1
-        if handLandmarks[16][1] < handLandmarks[14][1]:     #Ring finger
-          fingerCount = fingerCount+1
-        if handLandmarks[20][1] < handLandmarks[18][1]:     #Pinky
-          fingerCount = fingerCount+1
-
-        # Draw hand landmarks 
-        mp_drawing.draw_landmarks(
-            image,
-            hand_landmarks,
-            mp_hands.HAND_CONNECTIONS,
-            mp_drawing_styles.get_default_hand_landmarks_style(),
-            mp_drawing_styles.get_default_hand_connections_style())
-
-    # Display finger count
-    cv2.putText(image, str(fingerCount), (50, 450), cv2.FONT_HERSHEY_SIMPLEX, 3, (255, 0, 0), 10)
-
-    # Display image
-    cv2.imshow('MediaPipe Hands', image)
-    if cv2.waitKey(5) & 0xFF == 27:
-      break
 cap.release()
+cv2.destroyAllWindows()
